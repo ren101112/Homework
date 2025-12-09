a4325d43fd5b5014f3e72ad45c9b7c3ae79dd110
renhirose <ren_hirose28@milton.edu>
2025-12-08 20:30:41 -0500

    Graded homework for conditionals and reading the future

---
+++ src/Conditionals.java
@@ -0,0 +1,41 @@
+public class Conditionals {
+
+
+    public Conditionals() {
+
+
+        String question = "Will I pass my CS class?";
+        System.out.println(question);
+
+
+        int randomInt = (int)(Math.random() * 11);
+
+        //else if chain for each possible response
+        if (randomInt == 0) {
+            System.out.println("Without a doubt.");
+        } else if (randomInt == 1) {
+            System.out.println("Ask again later.");
+        } else if (randomInt == 2) {
+            System.out.println("Very doubtful.");
+        } else if (randomInt == 3) {
+            System.out.println("As I see it yes.");
+        } else if (randomInt == 4) {
+            System.out.println("Better not tell you.");
+        } else if (randomInt == 5) {
+            System.out.println("Signs point to yes.");
+        } else if (randomInt == 6) {
+            System.out.println("Dont count on it.");
+        } else if (randomInt == 7) {
+            System.out.println("outlook good.");
+        } else if (randomInt == 8) {
+            System.out.println("try again.");
+        } else {
+            System.out.println("Absolutely not.");
+        }
+    }
+
+//psvm
+    public static void main(String[] args) {
+        new Conditionals();
+    }
+}
